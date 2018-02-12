1c1b941d3378a5c8f01becdd648c3523c4939c13
Jay Park <jay.jongcheol.park@gmail.com>
2018-02-12 16:41:06 -0500

    Change index.html path

--- api-server/server.js
+++ api-server/server.js
@@ -322,7 +322,7 @@ if (process.env.NODE_ENV === 'production') {
     // if doesn't recognize the route
     const path = require('path')
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+      res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
     })
   }
 
